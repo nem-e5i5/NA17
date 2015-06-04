8958b70ea60a4a1990f32b345e33e28705acd9ca
yinsimei <roseline.yin@gmail.com>
2015-06-04 16:16:20 +0200

    version bien rangée

--- SQL/requetes.sql
+++ SQL/requetes.sql
@@ -1,36 +1,222 @@
--------------------------------------------------ADMINISTRATEUR-------------------------------------------------
 
---CREATION D'UN COMPTE
-INSERT INTO TUSER (login, firstname, lastname, aPassword) VALUES ('$newLogin','$newFirstName','$newLastName','$newPassword');
+-----------------------------PAGE D'ACCEUIL & PAGE DE LISTE-----------------------------------------------------------------
 
---On utilisera des checkbox pour renvoyer des "droit"?
---Si coché, on insère
-INSERT INTO DROIT_USER (login, droit) VALUES ('$newLogin','administrateur');
-INSERT INTO DROIT_USER (login, droit) VALUES ('$newLogin','auteur');
-INSERT INTO DROIT_USER (login, droit) VALUES ('$newLogin','editeur');
-INSERT INTO DROIT_USER (login, droit) VALUES ('$newLogin','moderateur');
-INSERT INTO DROIT_USER (login, droit) VALUES ('$newLogin','lecteur');
+-----------------LA LIST DES ARTICLES----------------------
 
--------------------------------------------------AUTEUR-------------------------------------------------------------
-
---CREATION D'UN ARTICLE--
-
-INSERT INTO ARTICLE (id, title, nbBloc, honor, aDate, author, statut) VALUES (idauto_art.nextval, '$newTitle', 0, 0, current_date, '$currentLogin', 'en_redaction'); --nextval???
+--LA LISTE DE TOUS LES ARTICLES
+SELECT A.id, A.title, A.honor, A.aDate, A.author, U.firstname, U.lastname, A.statut--Il faut vérifier que le statut='pubilie' pour les lecteur!!!
+FROM ARTICLE A, TUSER U
+WHERE A.author=U.login
+ORDER BY A.aDate;
 
 
---AJOUTER DES BLOCS--
---SI BLOC DE TEXT
-INSERT INTO BLOC (art, aOrder, title, texte, image_uml) VALUES ('$currentArt', fOrdreBloc($currentArt), '$titleBloc', '$nexText' , NULL);
---SI BLOC D'IMAGE
-INSERT INTO BLOC (art, aOrder, title, texte, image_uml) VALUES ('$currentArt', fOrdreBloc($currentArt), '$titleBloc', NULL, '$newUml')
 
 
---VOIR SES ARTICLES EN COURS DE REDACTION--
-SELECT id, title, aDate, statut
+--PAR RUBRIQUES ET/OU SOUS-RUBRIQUE
+----Rubriques qui n'ont pas de mère
+SELECT title
+FROM RUBRIQUE
+WHERE mother IS NULL;
+
+----Selon une Rubrique choisi, les titres de ses sous-rubriques
+SELECT title
+FROM RUBRIQUE
+WHERE mother='$currentRub';
+
+----la liste des articles par rubriques/sous-rubriques()
+SELECT A.id, A.title, A.honor, A.aDate, A.author, U.firstname, U.lastname, A.statut--Il faut vérifier que le statut='pubilie' pour les lecteur!!!
+FROM ARTICLE A, RUBRIQUE_ARTICLE RA, TUSER U
+WHERE A.id=RA.art AND RA.rub='$currentRub' AND A.author=U.login
+ORDER BY A.aDate;
+
+
+
+
+--PAR MOTS CLES
+----Tous les mots clés : ("datalist" de html sera bien pour le saisir https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist)
+SELECT word FROM TAGS;
+
+----
+SELECT A.id, A.title, U.firstName, U.lastname, A.aDate, A.honor, A.statut--CONDITION AVEC statut : Des lecteurs ET modérateurs ne peuvent que voir des articles 'publié' !!
+FROM ARTICLE A, TUSER U, TAGS T
+WHERE A.author=U.login AND T.art=A.id AND T.word = lower('$wordCherche');
+
+
+
+
+--PAR L'HONNEUR
+SELECT A.id, A.title, U.firstName, U.lastname, A.aDate, A.honor, A.statut----CONDITION AVEC statut : Des lecteurs ET modérateurs ne peuvent que voir des articles 'publié' !!
+FROM ARTICLE A, TUSER U
+WHERE A.author=U.login AND A.honor=1;
+
+
+
+
+----PAR AUTEUR
+------ la liste des auteurs
+SELECT A.author, U.firstName, U.lastname, COUNT(*) AS nbArticles
+FROM ARTICLE A, TUSER U
+WHERE A.author=U.login
+GROUP BY A.author, U.firstName, U.lastname
+ORDER BY A.author;
+
+------la liste des articles de l'auteur choisi
+SELECT A.id, A.title, U.firstName, U.lastname, A.aDate, A.honor, A.statut----CONDITION AVEC statut : Des lecteurs ET modérateurs ne peuvent que voir des articles 'publié' !!
+FROM ARTICLE A, TUSER U
+WHERE A.author=U.login AND A.author='$authorVoulu';
+
+
+
+
+----PAR DATE
+------ la liste des dates
+SELECT aDate, COUNT(*) AS nbArticles
 FROM ARTICLE
-WHERE author='$currentLogin' AND (statut='en_redaction' OR statut='supprime')
+GROUP BY aDate
 ORDER BY aDate;
 
+------la liste des articles de le la date choisi
+SELECT A.id, A.title, U.firstName, U.lastname, A.aDate, A.honor, A.statut
+FROM ARTICLE A, TUSER U
+WHERE A.aDate = '$dateVoulu' AND A.author=U.login
+ORDER BY aDate;
+
+
+
+
+----PAR STATUT (Pour un author ou un éditeur)
+------ la liste des statuts
+SELECT statut, COUNT(*) AS nbArticles
+FROM ARTICLE
+GROUP BY statut
+ORDER BY statut;
+
+------la liste des articles de du statut choisi
+SELECT A.id, A.title, U.firstName, U.lastname, A.aDate, A.honor, A.statut
+FROM ARTICLE A, TUSER U
+WHERE A.statut = '$statutVoulu' AND A.author=U.login
+ORDER BY A.aDate;
+
+--VOIR LES ARTICLES EN COURS DE REDACTION DE L'AUTEUR
+SELECT A.id, A.title, U.firstName, U.lastname, A.aDate, A.honor, A.statut
+FROM ARTICLE A, TUSER U
+WHERE A.author='$currentLogin' AND (A.statut='en_redaction' OR A.statut='supprime') AND A.author=U.login
+ORDER BY A.aDate; --L'author peut seulement modifier ses articles en rédaction
+
+
+
+-----------------LIER DEUX ARTICLES (POUR UN EDIREUR)----------------------
+INSERT INTO TIE_ARTICLE (firstArticle, secondArticle) VALUES ('$art1','$art2');
+
+
+
+
+-----------------CREATION D'UN ARTICLE (POUR UN AUTEUR)--------------------------------
+INSERT INTO ARTICLE (id, title, nbBloc, honor, aDate, author, statut) VALUES (nextval('idauto_art'), '$newTitle', 0, 0, current_date, '$currentLogin', 'en_redaction');
+
+
+
+
+
+----------------------------------PAGE D'INSCRIPTION-------------------------------------------------------------------------------
+--CREATION D'UN COMPTE
+INSERT INTO TUSER (login, firstname, lastname, aPassword, droit) VALUES ('$newLogin','$newFirstName','$newLastName','$newPassword', 'lecteur');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-------------------------PAGE POUR DONNER LE DROIT (ADMINISTRATEUR)-----------------------------------------------------------------
+UPDATE TUSER SET droit = 'lecteur' WHERE login = '$currentLogin';
+UPDATE TUSER SET droit = 'editeur' WHERE login = '$currentLogin';
+UPDATE TUSER SET droit = 'auteur' WHERE login = '$currentLogin';
+UPDATE TUSER SET droit = 'moderateur' WHERE login = '$currentLogin';
+UPDATE TUSER SET droit = 'administrateur' WHERE login = '$currentLogin';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+------------------------------------------------------PAGE D'ARTICLE-----------------------------------------------------------------
+
+--VISUALISER LES ARTICLES
+SELECT A.title, U.firstName, U.lastname, A.aDate, A.honor, A.statut
+FROM ARTICLE A, TUSER U
+WHERE A.id='$currentArt' AND A.author=U.login;
+
+----afficher des blocs par ordre
+SELECT art, aOrder, title, texte, image_uml
+FROM BLOC
+WHERE art='$currentArt' 
+ORDER BY aOrder;
+
+
+
+
+--ACCEDER AUX ARTICLE LIES DEPUIS UN ARTICLE LU
+--plus de travail au niveau de php
+--les titres des articles associés
+SELECT A.title, A.statut--CONDITION AVEC statut: Des lecteurs ET modérateurs ne peuvent que voir des articles 'publié'
+FROM ARTICLE A
+WHERE A.id IN
+(SELECT firstArticle FROM TIE_ARTICLE T WHERE T.secondArticle='$currentArt'
+UNION
+SELECT secondArticle FROM TIE_ARTICLE T WHERE T.firstArticle='$currentArt');
+
+
+
+
+--AJOUTER UN COMMENTAIRE A UN ARTICLE
+INSERT INTO COMMENTAIRE(id, art, aDate, creator, texte, statut) VALUES(nextval('idauto_comm'), '$currentArt', current_timestamp ,'$currentLogin', '$newComm', 'visible');
+
+--SUPPRIMER SES COMMENTAIRE
+UPDATE COMMENTAIRE SET statut='supprime' WHERE id='$currentComm' AND creator='$currentLogin';
+
+--LIRE DES COMMENTAIRES(NON MASQUE) DES AUTRES
+--CONDITION pour lecteur : non masque : (statut = 'exergue' OR statut = 'visible') !!
+--Des modérateurs peuvent voir tous les commantaires !!
+SELECT aDate, creator, texte, statut
+FROM COMMENTAIRE
+WHERE art='$currentArt'
+ORDER BY aDate;
+
+
+
+
+----------------POUR UN MODERATEUR
+UPDATE COMMENTAIRE SET statut='supprime' WHERE id='$currentComm';
+UPDATE COMMENTAIRE SET statut='visible' WHERE id='$currentComm';
+UPDATE COMMENTAIRE SET statut='exergue' WHERE id='$currentComm';
+UPDATE COMMENTAIRE SET statut='masque' WHERE id='$currentComm';
+
+
+
+---------------- POUR UN AUTEUR
 
 --SUPPRIMER UN ARTICLE--
 UPDATE ARTICLE SET statut='supprime' WHERE id='$currentArt';
@@ -42,65 +228,7 @@ UPDATE ARTICLE SET statut='en_redaction' WHERE id='$currentArt' AND statut='supp
 UPDATE ARTICLE SET statut='soumis' WHERE id='$currentArt' AND statut='en_redaction';
 
 
-
--------------------------------------------------EDITEUR----------------------------------------------------------
-
---VOIR ENSEMBLE DES ARTICLE, PAR AUTEUR, PAR DATE, PAR STATUT
-
-----PAR AUTEUR
------- la liste des auteurs
-SELECT A.author, U.firstName, U.lastname, COUNT(*) AS nbArticles
-FROM ARTICLE A, TUSER U
-WHERE A.author=U.login
-GROUP BY A.author, U.firstName, U.lastname
-ORDER BY A.author;
-
-------la liste des articles de l'auteur choisi
-SELECT id, title, honor, aDate, statut
-FROM ARTICLE
-WHERE id = '$authorVoulu'
-ORDER BY aDate;
-
-
-----PAR DATE
------- la liste des dates
-SELECT aDate, COUNT(*) AS nbArticles
-FROM ARTICLE
-GROUP BY aDate
-ORDER BY aDate;
-
-------la liste des articles de le la date choisi
-SELECT id, title, honor, aDate, statut
-FROM ARTICLE
-WHERE aDate = '$dateVoulu'
-ORDER BY aDate;
-
-
-----PAR STATUT
------- la liste des statuts
-SELECT statut, COUNT(*) AS nbArticles
-FROM ARTICLE
-GROUP BY statut
-ORDER BY statut;
-
-------la liste des articles de du statut choisi
-SELECT id, title, honor, aDate, statut
-FROM ARTICLE
-WHERE statut = '$statutVoulu'
-ORDER BY aDate;
-
---ACCEDER AUX ARTICLES ET LES CORRIGER
---visualiser l'article
-----l'information de l'article(title, author, date, statut)
-SELECT A.title, U.firstName, U.lastname, A.aDate, A.statut, A.honor
-FROM ARTICLE A, TUSER U
-WHERE A.id='$currentArt' AND A.author=U.login;
-
-----afficher des blocs par ordre
-SELECT art, aOrder, title, texte, image_uml
-FROM BLOC
-WHERE art='$currentArt' 
-ORDER BY oOrder;
+---------------- POUR UN EDITEUR
 
 ----afficher la justification si rejete
 SELECT justification
@@ -114,13 +242,7 @@ WHERE art='$currentArt'
 ORDER BY aDate;
 
 
-
---corriger l'article/bloc
-UPDATE BLOC SET texte ='$newText' WHERE art='$currentArt' AND aOrder='$currentOrder';
-
-
 --ASSOCIER UN STATUT A CHAQUE ARTICLE
---checkbox?
 UPDATE ARTICLE SET statut='en_redaction' WHERE id='$currentArt';
 UPDATE ARTICLE SET statut='en_relecture' WHERE id='$currentArt';
 
@@ -137,12 +259,29 @@ INSERT INTO PRECONISATION (art, aDate, editor, texte) VALUES ('$currentArt', cur
 INSERT INTO TAGS (art, word) VALUES('$currentArt', '$newTag');
 
 
+
+
+------------------------------------------------------PAGE DU BLOC-----------------------------------------------------------------
+--corriger des blocs (EDITEUR)
+UPDATE BLOC SET texte ='$newText' AND title = '$newText' WHERE art='$currentArt' AND aOrder='$currentOrder';
+
+--insérer un bloc (AUTEUR)
+--SI BLOC DE TEXT
+INSERT INTO BLOC (art, aOrder, title, texte, image_uml) VALUES ('$currentArt', fOrdreBloc($currentArt), '$titleBloc', '$nexText' , NULL);
+--SI BLOC D'IMAGE
+INSERT INTO BLOC (art, aOrder, title, texte, image_uml) VALUES ('$currentArt', fOrdreBloc($currentArt), '$titleBloc', NULL, '$newUml');
+
+
+
+
+
+
+
+------------------------------------------------------PAGE DU RUBRIQUE-----------------------------------------------------------------
 --CREATION D'UN RUBRIQUE
---On utilisera des checkbox pour renvoyer des "mère"?
---Si coché, on insère une mère ?
 INSERT INTO RUBRIQUE (title, mother, creator, aDate) VALUES ( '$newTitle', '$mother', '$currentLogin', current_timestamp);
 
---ASSOCIER DES ARTICLES A UNE OU PLUSIEURS RUBRIQUES
+--ASSOCIER DES ARTICLES A UNE RUBRIQUES
 INSERT INTO RUBRIQUE_ARTICLE (rub, art) VALUES('$rubriqueVoulu','$currentArt');
 
 --SELECTIONNER PARMI DES ARTICLES VALIDES CEUX QUI SONT PUBILIE SUR LE SITE
@@ -155,70 +294,5 @@ UPDATE ARTICLE SET statut='publie' WHERE id='$currentArt';
 --PROPOSER UNE CATEGORIE A L'HONNEUR
 UPDATE ARTICLE SET honor=1 WHERE id='$currentArt';
 
---LIER DEUX ARTICLES
-INSERT INTO TIE_ARTICLE (firstArticle, secondArticle) VALUES ($art1,$art2);
 
 
--------------------------------------------------MODERATEUR----------------------------------------------------------
-
---On utilisera des liens pour renvoyer des fonctions?
-UPDATE COMMENTAIRE SET statut='supprime' WHERE id='$currentComm';
-UPDATE COMMENTAIRE SET statut='visible' WHERE id='$currentComm';
-UPDATE COMMENTAIRE SET statut='exergue' WHERE id='$currentComm';
-UPDATE COMMENTAIRE SET statut='masque' WHERE id='$currentComm';
-
-
--------------------------------------------------LECTEUR----------------------------------------------------------
-
---VISUALISER LES ARTICLES
-----l'information de l'article(title, author, date), on n'affiche pas "statut" ici.
-SELECT A.title, U.firstName, U.lastname, A.aDate, A.honor
-FROM ARTICLE A, TUSER U
-WHERE A.id='$currentArt' AND A.author=U.login;
-
-----afficher des blocs par ordre
-SELECT art, aOrder, title, texte, image_uml
-FROM BLO
-WHERE art='$currentArt' 
-ORDER BY oOrder;
-
-
---ACCEDER AUX ARTICLES PAR RUBRIQUES ET/OU SOUS-RUBRIQUE
-----Selon une Rubrique choisi, afficher les titres de ses sous-rubriques
-SELECT title
-FROM RUBRIQUE
-WHERE mere='$currentRub';
-
-----la liste des articles par rubriques/sous-rubriques
-SELECT A.id, A.title, A.honor, A.aDate, A.author
-FROM ARTICLE A, RUBRIQUE_ARTICLE RA
-WHERE A.id=RA.art AND (RA.rub='$currentRub' OR RA.rub IN (SELECT title FROM RUBRIQUE WHERE mere='$currentRub'))
-ORDER BY aDate;
---??? ça marche seulement pour des rubriques de 2 niveaux, on fait comment, pour affichier des articles des
---??? sous-briques d'un rubrique ? on cree la lien entre la rubrique et l'article quand cet article est lié 
---??? à son sous-rubrique ??? Ou bien on construire ces liens à la main ? 
-
-
---EFFECTUER UNE RECHERCHE PAR MOTS CLES
-
-
---ACCEDER A LA CATEGORIE A L'HONNEUR
-
-
---ACCEDER AUX ARTICLE LIES DEPUIS UN ARTICLE LU
---plus de travail au niveau de php
---les titres des articles associés
-
-
---ASSOCIER UN COMMENTAIRE A UN ARTICLE
-INSERT INTO COMMENTAIRE(id, art, aDate, creator, texte, statut) VALUES(idauto_comm.nextval, '$currentArt', current_timestamp ,'$currentLogin', '$newComm', 'visible');
-
-
---SUPPRIMER SES COMMENTAIRE
-UPDATE COMMENTAIRE SET statut='supprime' WHERE id='$currentComm' AND creator='$currentLogin';
-
---LIRE DES COMMENTAIRES(NON MASQUE) DES AUTRES
-SELECT aDate, creator, texte, statut
-FROM COMMENTAIRE
-WHERE art='$currentArt' AND (statut = 'exergue' OR statut = 'visible')
-ORDER BY aDate;
