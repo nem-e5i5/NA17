ad4cd84bb05a7e6521865dd15033c8964789de86
yinsimei <roseline.yin@gmail.com>
2015-06-06 09:27:01 +0200

    Ameliorer la mis en page

--- PHP/inscription.php
+++ PHP/inscription.php
@@ -1,70 +1,94 @@
 <html>
-<head><title>Autorisation</title></head>
+<head><title>Inscription</title></head>
+<style>
+.error {color: #FF0000;}
+</style>
 <body>
-<h2>Autoriser un utilisateur</h2>
+
 <?php
 include "connect.php";
 $vConn = fConnect();
-?>
-	<p><table border="1">
-		<tr>
-			<td width="100pt">
-				<b>Login</b>
-			</td>
-			<td width="300pt">
-				<b>Nom</b>
-			</td>
-			<td width="150pt">
-				<b>Droit</b>
-			</td>
-			<td width="60pt">
-				<b>Choix</b>
-			</td>	
-		</tr>
-<?php
-	$vSql ="SELECT login, firstName, lastname, droit FROM TUSER ORDER BY login;";
+// define variables and set to empty values
+$loginErr = $mdpErr = "";
+$login = $nom = $prenom = $mdp = "";
+$succes1=$succes2=0;
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["login"])) {
+     $loginErr = "Champs obligatoire";
+   }
+   else {
+     $login=$_POST["login"];
+     if (strlen($login)>30) {
+       $loginErr = "Moins de 30 caracteres, s'il vous plait";
+     }
+     else {
+	if(strlen($login)<7){
+	   $loginErr = "Au moins 7 caracteres, s'il vous plait";
+	}
+	else {
+	    if (!ctype_alnum($login)){
+		$loginErr = "Que des lettres et chiffres, s'il vous plait</font>";
+	    }
+	    else{
+		$vSql ="SELECT login FROM TUSER WHERE login= '$login';";
+		$vQuery=pg_query($vConn, $vSql);
+		$vResult = pg_fetch_array($vQuery);
+		if($vResult['login']!=NULL){
+			$loginErr = "Le login existe deja";
+	    	}
+		else{$succes1=1;}
+	    }
+        }
+    }
+   }
+    if (!empty($_POST["nom"]))$nom=$_POST["nom"];
+    if (!empty($_POST["prenom"]))$prenom=$_POST["prenom"];
+
+    if (empty($_POST["mdp"])) {
+     $mdpErr = "Champs obligatoire";
+   } else {
+     $mdp=$_POST["mdp"];
+     if (strlen($mdp)<8) {
+       $mdpErr = "Au moins 8 caracteres, s'il vous plait";
+     }
+     else {
+	if (preg_match("/\\s/", $mdp)) {
+		$mdpErr = "Pas d'epace, s'il vous plait";
+	}
+	else{$succes2=1;}
+
+     }
+   }
+
+}
+	if(($succes1==1) and ($succes2==1)){
+	$vSql="INSERT INTO TUSER (login, firstname, lastname, aPassword, droit) VALUES ('$login','$prenom','$nom','$mdp', 'lecteur');";
 	$vQuery=pg_query($vConn,$vSql);
-	$vResult = pg_fetch_array($vQuery);
-	echo"<form method='post' action=''>" ;
-	while ($vResult = pg_fetch_array($vQuery)){
-		echo "<tr>";
-		echo "<td>$vResult[login]</td>";
-		echo "<td>$vResult[firstname]"." "."$vResult[lastname]"."</td>";
-		echo "<td>$vResult[droit]</td>";
-		echo "<td>
-			<input type='radio' name='login' value="."$vResult[login]".">
-			</td>";
-		echo "</tr>";
 	}
-	
-?>
-	</table>
-	
-	<p><select name="droit">
-	<option value="administrateur">Administrateur</option> 
-	<option value="editeur">Editeur</option>
-	<option value="auteur">Auteur</option>
-	<option value="lecteur"selected>Lecteur</option>
-	<option value="moderateur">Moderateur</option>
-	</select>
-
-	<input type="submit" name="submit" value="Autoriser" /> 
-	</form></p>
-
-<?php
-	
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		if (!empty($_POST["login"]) and !empty($_POST["droit"])){
-			$login=$_POST["login"];
-			$droit=$_POST["droit"];
-			$vSql ="UPDATE TUSER SET droit = '$droit' WHERE login = '$login';";
-			$vQuery=pg_query($vConn,$vSql);
-			Header("Location: autorisation.php");
-		}
-		else echo "<font size='3' color='red'>Choisissez, s'il vous plait.";
-	}
+	if($succes1==1 && $succes2==1){Header("Location: acceuil.php?login=".$login);}
 pg_close($vConn);
 ?>
-<p><a href="acceuil.php">Retour</a></p>
+
+<h2>Creer un nouveau compte</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="">
+   Login(7~30 caracteres): <input type="text" name="login" value="<?php echo $login;?>">
+   <span class="error">* <?php echo $loginErr;?></span>
+   <br><br>
+   Nom: &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 
+   <input type="text" name="nom" value="<?php echo $nom;?>">
+   <br><br>
+   Prenom:  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp&nbsp 
+   <input type="text" name="prenom" value="<?php echo $prenom;?>">
+   <br><br>
+   Mot de passe:&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 
+   <input type="password" name="mdp" value="<?php echo $mdp;?>">
+   <span class="error">* <?php echo $mdpErr;?></span>
+   <br><br>
+   <input type="submit" name="submit" value="Submit">
+   <p><a href="acceuil.php">Retour</a></p>
+</form>
 </body>
 </html>
